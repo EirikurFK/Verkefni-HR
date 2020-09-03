4733043e9c8acef44e311c61d0e040004d5690bf
Eiríkur Friðjón Kjartansson <eirikurfk@gmail.com>
2020-09-03 10:11:53 +0000

    Algorithm created

--- sequence.py
+++ sequence.py
@@ -2,6 +2,14 @@
 # in the following sequence:
 # ; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
 
-#
+# Store the first 3 numbers of the sequence.
+# Add the last 3 numbers together to create a fourth number
+# Repeat
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+number_1 = 1
+number_2 = 1
+number_3 = 1
+
+print number
