a2b33da011421e0a8af15ecfbb986a0df471d771
Eiríkur Friðjón Kjartansson <eirikurfk@gmail.com>
2020-09-03 10:29:19 +0000

    Prototype number 1 finished

--- sequence.py
+++ sequence.py
@@ -5,11 +5,31 @@
 # Store the first 3 numbers of the sequence.
 # Add the last 3 numbers together to create a fourth number
 # Repeat
+# 0,0,0 = 0
+# 0,0,1 = 1
+# 0,1,1 = 2
+# 1,1,1 = 3
+# 1,2,3 = 6
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-number_1 = 1
-number_2 = 1
-number_3 = 1
+count = 1
+while count <= n:
+    number1 = 0
+    number2 = 0
+    number3 = 1
+    number4 = number1 + number2 + number3
+    print(number4, ", ")
+    if number1 = 0 and number4 != 1:
+        number1 = 1
+    else:
+        number1 = number2
 
-print number
+    if number2 = 0:
+        number2 = 1
+    else:
+        number2 = number3
+
+    number3 = number4
+
+    count += 1
