fed8c5ea1205d4846a66f472d8ab62b781c19598
Eiríkur Friðjón Kjartansson <eirikurfk@gmail.com>
2020-09-03 11:17:43 +0000

    Program complete

--- sequence.py
+++ sequence.py
@@ -1,35 +1,26 @@
-# Design an algorithm that generates the first n numbers
-# in the following sequence:
-# ; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
-
 # Store the first 3 numbers of the sequence.
 # Add the last 3 numbers together to create a fourth number
 # Repeat
-# 0,0,0 = 0
-# 0,0,1 = 1
-# 0,1,1 = 2
-# 1,1,1 = 3
-# 1,2,3 = 6
-
+# 1: 0,0,1 = 1
+# 2: 0,1,1 = 2
+# 3: 0,1,2 = 3
+# 4: 1,2,3 = 6
+# 5: 2,3,6 = 11
+# 6: 3,6,11 = 20
+# 7: 6,11,20 = 37
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-count = 1
-while count <= n:
-    number1 = 0
-    number2 = 0
-    number3 = 1
-    number4 = number1 + number2 + number3
-    print(number4, ", ")
-    if number1 = 0 and number4 != 1:
-        number1 = 1
-    else:
-        number1 = number2
+n1 = 0
+n2 = 1
+n3 = 0
+count = 0
 
-    if number2 = 0:
-        number2 = 1
-    else:
-        number2 = number3
-
-    number3 = number4
+while count < n:
+    nt = n1 + n2 + n3
+    print(nt)
+    n1 = n2
+    n2 = n3
+    n3 = nt
+    
 
     count += 1
